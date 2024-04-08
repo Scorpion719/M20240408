f27038428af8d19632da0165b5639dd6579b8c09
Emerson neftali Aguilar Erazo <ae24001@esfe.agape.edu.sv>
2024-04-08 10:27:49 -0600

    Validacion

--- M20240408.EntidadesDeNegocio/PersonaM.cs
+++ M20240408.EntidadesDeNegocio/PersonaM.cs
@@ -13,19 +13,23 @@ namespace M20240408.EntidadesDeNegocio
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        [Required]
+       
         [StringLength(60)]
         [Display(Name ="Emerson")]
+        [Required(ErrorMessageResourceName ="El campo Nombre es Requerido")]
         public string NombreM { get; set; }
-        [Required]
+        [Required(ErrorMessageResourceName = "El campo Apellido es Requerido")]
         [StringLength(60)]
         [Display(Name = "Aguilar")]
         public string ApellidoM { get; set; }
+        [Required(ErrorMessageResourceName = "El campo Fecha Nacimiento es Requerido")]
         [Display(Name = "Fecha Nacimiento")]
         public DateTime FechaNacimientoM { get; set; }
         [Display(Name = "Sueldo")]
+        [Required(ErrorMessageResourceName = "El campo Sueldo es Requerido")]
         public decimal SueldoM { get; set; }
         [Display(Name = "Status")]
+        [Required(ErrorMessageResourceName = "El campo Status es Requerido")]
         public byte StatusM { get; set; }
 
     }
