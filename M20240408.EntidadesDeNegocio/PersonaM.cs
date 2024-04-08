8c5b0baa79f226c9301ddf135ec1e9a9e5745033
Emerson neftali Aguilar Erazo <ae24001@esfe.agape.edu.sv>
2024-04-08 10:16:04 -0600

    Terminado

--- M20240408.EntidadesDeNegocio/PersonaM.cs
+++ M20240408.EntidadesDeNegocio/PersonaM.cs
@@ -15,12 +15,17 @@ namespace M20240408.EntidadesDeNegocio
         public int Id { get; set; }
         [Required]
         [StringLength(60)]
+        [Display(Name ="Emerson")]
         public string NombreM { get; set; }
         [Required]
         [StringLength(60)]
+        [Display(Name = "Aguilar")]
         public string ApellidoM { get; set; }
+        [Display(Name = "Fecha Nacimiento")]
         public DateTime FechaNacimientoM { get; set; }
+        [Display(Name = "Sueldo")]
         public decimal SueldoM { get; set; }
+        [Display(Name = "Status")]
         public byte StatusM { get; set; }
 
     }
